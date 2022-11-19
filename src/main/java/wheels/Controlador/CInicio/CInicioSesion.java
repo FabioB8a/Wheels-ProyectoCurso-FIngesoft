8537017b39ea76db3fa4665923ad4d909f7722c2
FabioB8a <fabiobuitra@gmail.com>
2022-11-19 12:19:13 -0500

    Cambio fxml

--- src/main/java/wheels/Controlador/CInicio/CInicioSesion.java
+++ src/main/java/wheels/Controlador/CInicio/CInicioSesion.java
@@ -2,19 +2,29 @@ package wheels.Controlador.CInicio;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import wheels.Controlador.CEscenarios;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CInicioSesion {
+public class CInicioSesion implements Initializable {
     @FXML
     private PasswordField contrasenia;
 
     @FXML
     private TextField correoElectronico;
+
+    @FXML
+    private RadioButton bConductor;
+
+    @FXML
+    private RadioButton bPasajero;
 
     @FXML
     void btnInicioSesion(ActionEvent event) {
@@ -26,4 +36,8 @@ public class CInicioSesion {
         CEscenarios.cambiarVista(event,"VistaPrincipal.fxml");
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
